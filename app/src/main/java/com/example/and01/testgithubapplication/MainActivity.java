1eba917a563011d05fbf23898691b25004827e45
mamatha <mamatha.k@anaghasoftech.com>
2020-01-17 14:45:58 +0530

    log added

--- app/src/main/java/com/example/and01/testgithubapplication/MainActivity.java
+++ app/src/main/java/com/example/and01/testgithubapplication/MainActivity.java
@@ -17,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i("test log","hii");
 
+
         Toast.makeText(MainActivity.this,"Test Toast",Toast.LENGTH_SHORT).show();
+        Log.i("test log","hii");
         
     }
 }
