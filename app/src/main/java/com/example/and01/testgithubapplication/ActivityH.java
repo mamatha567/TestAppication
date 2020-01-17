2942ecf1fe5675912b22a8f249b440b839d170c6
harsha <harsha@anaghasoftech.com>
2020-01-17 16:54:41 +0530

    activity h implementaion

--- app/src/main/java/com/example/and01/testgithubapplication/ActivityH.java
+++ app/src/main/java/com/example/and01/testgithubapplication/ActivityH.java
@@ -1,6 +1,17 @@
 package com.example.and01.testgithubapplication;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class ActivityH extends AppCompatActivity {
+    Button bth;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activityh);
+        bth = (Button) findViewById(R.id.btn);
+    }
 }
