c39ad3e8ff32742a05d9da65120dc5e50aa13207
mamatha <mamatha.k@anaghasoftech.com>
2020-01-17 14:42:51 +0530

    textview added

--- app/src/main/java/com/example/and01/testgithubapplication/MainActivity.java
+++ app/src/main/java/com/example/and01/testgithubapplication/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.and01.testgithubapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
