ec69cdd6d526c94f8383c769382577a2a58996c5
harsha <harsha@anaghasoftech.com>
2020-01-17 14:43:30 +0530

    Toast added

--- app/src/main/java/com/example/and01/testgithubapplication/MainActivity.java
+++ app/src/main/java/com/example/and01/testgithubapplication/MainActivity.java
@@ -16,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
         Log.i("test log","hii");
 
         Toast.makeText(MainActivity.this,"Test Toast",Toast.LENGTH_SHORT).show();
+        
     }
 }
