6dd057bc306650d1e05f457bd499ec62c2d9a866
mamatha <mamatha.k@anaghasoftech.com>
2020-01-17 16:54:50 +0530

    TestActivity imaplementaion

--- app/src/main/java/com/example/and01/testgithubapplication/TestActivity.java
+++ app/src/main/java/com/example/and01/testgithubapplication/TestActivity.java
@@ -3,24 +3,35 @@ package com.example.and01.testgithubapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class TestActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView textView;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         textView = findViewById(R.id.textview);
+        button  = findViewById(R.id.btn_text);
         textView.setOnClickListener(this);
+        button.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.textview:
+                Toast.makeText(this, "TestActivity", Toast.LENGTH_SHORT).show();
+
+                Toast.makeText(this, "TestActivity", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_text:
+
+
                 Toast.makeText(this, "TestActivity", Toast.LENGTH_SHORT).show();
                 break;
 
